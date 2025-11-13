2a539cc314db0e36fb13a959d7bc0499e3ced962
JuanNavarrete27 <jnavarretealdave@gmail.com>
2025-11-13 17:09:39 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-
 /*
   db.js - mysql2 pool connection
 */
 const mysql = require('mysql2');
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bentasca_usuarios',
+  host: process.env.MYSQL_ADDON_HOST || 'b99rg5klb9i5qhzdwcy4-mysql.services.clever-cloud.com',
+  user: process.env.MYSQL_ADDON_USER || 'uanhygqsgszwryv8',
+  password: process.env.MYSQL_ADDON_PASSWORD || 'vpP9Puizq27MwNCL7PrZ',
+  database: process.env.MYSQL_ADDON_DB || 'b99rg5klb9i5qhzdwcy4',
+  port: process.env.MYSQL_ADDON_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
