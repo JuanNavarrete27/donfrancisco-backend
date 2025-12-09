74daa547a8d15e96952ecad9794340082b3dcbec
JuanNavarrete27 <jnavarretealdave@gmail.com>
2025-12-09 17:42:17 -0300

    Update soloAdmin.js

--- middlewares/soloAdmin.js
+++ middlewares/soloAdmin.js
@@ -1,9 +1,6 @@
-
-/*
-  middlewares/soloAdmin.js - allow only when req.user.rol === 'admin'
-*/
-module.exports = function(req, res, next) {
-  if (!req.user) return res.status(401).json({ error: 'No user info' });
-  if (req.user.rol !== 'admin') return res.status(403).json({ error: 'Solo admin' });
+module.exports = (req, res, next) => {
+  if (!req.user) return res.status(401).json({ error: "No user" });
+  if (req.user.rol !== "admin")
+    return res.status(403).json({ error: "Solo admin" });
   next();
 };
