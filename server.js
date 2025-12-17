e493d9e4b19e420306863dae4bad98e950088a6e
JuanNavarrete27 <jnavarretealdave@gmail.com>
2025-12-17 00:52:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:4200",
-  "https://donfrancisco.uy/"
+  "https://donfrancisco.uy"
 ].filter(Boolean);
 
 app.use(
